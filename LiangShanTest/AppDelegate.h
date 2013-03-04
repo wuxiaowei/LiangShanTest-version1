4be55b5533ee3050f06ef38acd8e13cf0e8c301b
wuxiaowei <wuxiaowei@outlook.com>
2013-03-04 09:49:06 +0800

    Initial Commit

---
+++ LiangShanTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiangShanTest
+//
+//  Created by wu xiaowei on 13-3-4.
+//  Copyright (c) 2013年 wu xiaowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
